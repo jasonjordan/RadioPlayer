ba96acf93050b42b0173547243d699a43539e782
jasonjordan <jason.jordan@gmail.com>
2026-05-19 18:19:29 +0800

    Change radio name and update streaming URL

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const RADIO_NAME = 'Jailson Web Rádio';
+const RADIO_NAME = 'Happy Radio'
 
 // Change Stream URL Here, Supports, ICECAST, ZENO, SHOUTCAST, RADIOJAR and any other stream service.
-const URL_STREAMING = 'https://stream.zeno.fm/yn65fsaurfhvv';
+const URL_STREAMING = 'https://hello.citrus3.com:8410';
 
 //API URL /
 const API_URL = 'https://twj.es/free/?url='+URL_STREAMING;
